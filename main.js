16d9611ce8e234fe0ca677dd6d45214a59f47210
Agustin Landa <93983553+Landa86@users.noreply.github.com>
2024-05-20 13:47:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -61,3 +61,71 @@ instagramContent.forEach((item) => {
   instagram.appendChild(duplicateNode);
 });
 
+
+// about form
+
+document.addEventListener("DOMContentLoaded", function () {
+  const formulario = document.getElementById("formulario");
+  const successMessage = document.getElementById("mensaje-enviado");
+
+  successMessage.style.display = 'none';
+
+  function validarFormulario(event) {
+      event.preventDefault();
+
+      const nombre = document.getElementById("nombre").value;
+      const apellido = document.getElementById("apellido").value;
+      const sexo = document.getElementById("sexo").value;
+      const correo = document.getElementById("correo").value;
+      const aceptarTerminos = document.getElementById("aceptar-terminos").checked;
+
+      
+      const errores = document.querySelectorAll(".error");// Limpiar errores anteriores
+      errores.forEach(error => error.innerHTML = "");
+
+      let formValid = true;
+
+      if (nombre.trim().length < 3) {
+          document.getElementById("error-nombre").innerHTML = "El nombre debe tener al menos 3 caracteres";
+          formValid = false;
+      }
+        
+
+      if (apellido.trim().length < 3) {
+          document.getElementById("error-apellido").innerHTML = "El apellido debe tener al menos 3 caracteres";
+          formValid = false;
+      }
+        
+      if (!sexo) {
+          document.getElementById("error-sexo").innerHTML = "Seleccione una opción de sexo";
+          formValid = false;
+      
+      }
+
+      const correoRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!correoRegExp.test(correo)) {
+          document.getElementById("error-correo").innerHTML = "Ingrese un correo electrónico válido";
+          formValid = false;
+      }
+
+      if (!aceptarTerminos) {
+          document.getElementById("error-terminos").innerHTML = "Debe aceptar los términos y condiciones";
+          formValid = false;
+      
+        }
+
+   if (formValid) {
+          formulario.style.display = 'none'; // Oculta el formulario
+          successMessage.style.display = 'block'; // Muestra el mensaje de éxito
+
+          setTimeout(() => {
+            successMessage.style.display = 'none'; 
+              formulario.style.display = 'block'; 
+              formulario.reset(); 
+          }, 3000);
+      }
+  }
+
+  formulario.addEventListener("submit", validarFormulario)
+
+});
